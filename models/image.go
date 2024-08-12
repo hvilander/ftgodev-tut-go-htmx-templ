d7e65e57be3f88d77f1262567bffa85532d971ca
Harold <harold.vilander@gmail.com>
2024-08-12 07:33:32 -0500

    Added image model and migration script

---
+++ models/image.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+  "time"
+  "github.com/google/uuid"
+)
+
+type ImageStatus int
+
+const (
+  ImageStatusFailed ImageStatus = iota
+)
+
+type Image struct {
+  ID int `bun:"id,pk,autoincrement"`
+  UserID uuid.UUID
+  Status ImageStatus
+  ExternalID string
+  Prompt string 
+  deleted bool `bun:"default:'false'"`
+  DeletedAt time.Time
+  CreatedAt time.Time `bun:"default 'now()'"`
+}
