2d9832269e712cd3959d69223179c024557857b3
Harold <harold.vilander@gmail.com>
2024-08-22 06:50:04 -0500

    migration example credits to accounts

--- handler/settings.go
+++ handler/settings.go
@@ -1,27 +1,23 @@
 package handler 
 
 import (
-  "fmt"
   "net/http"
   "ftgodev-tut/db"
   "ftgodev-tut/view/settings"
   "ftgodev-tut/pkg/kit/validate"
 )
 
-
 func HandleSettingsIndex(w http.ResponseWriter, r *http.Request) error {
   // make sure user is authed
   user := getAuthenticatedUser(r)
 
   return render(r, w, settings.Index(user))
-
 }
 
 func HandleProfile(w http.ResponseWriter, r *http.Request) error {
   params := settings.ProfileParams{
     Username: r.FormValue("username"),
   }
-  fmt.Println("HELO from Profile update")
   errors := settings.ProfileFormErrors{}
 
   ok := validate.New(&params, validate.Fields{
--- models/account.go
+++ models/account.go
@@ -6,8 +6,9 @@ import (
 )
 
 type Account struct {
-  ID int `bun:"id,pk,autoincrement"`
-  UserID uuid.UUID
-  Username string
+  ID int    `bun:"id,pk,autoincrement"`
+  UserID    uuid.UUID
+  Username  string
+  Credits   int
   CreatedAt time.Time `bun:"default:'now()'"`
 }
